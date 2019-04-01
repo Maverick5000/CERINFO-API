3266eba07f992d8fbddf6c3aa0d1f811c9e405f8
André Kraüss <andre.kmbv@gmail.com>
2019-04-01 01:42:28 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  resources :usuarios
+  resources :sigtops
+  resources :prestamos
+  resources :materials
+  resources :idiomas
+  resources :generos
+  resources :editorials
+  resources :bloqueos
+  resources :autors
+  resources :libros
+  resources :multas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
