822d93cc40faf67e763634ca9d448da0ba62065d
André Kraüss <andre.kmbv@gmail.com>
2019-04-02 23:31:03 -0400

    Revert

--- app/controllers/libros_controller.rb
+++ app/controllers/libros_controller.rb
@@ -4,6 +4,7 @@ class LibrosController < ApplicationController
   # GET /libros
   def index
     #@libros = Libro.all
+    #
     @libros = Libro.joins(:autor, :genero, :idioma, :material, :sigtop, :editorial).select("libros.id, titulo_libro, tomo_libro, area_libro, edicion_libro, ano_libro, lugar_publicacion_libro, ano_publicacion_libro, nombre_autor, nombre_genero, significado_idioma, significado_material, localidad_sigtop, dewey_sigtop, cuter_sigtop, nombre_editorial")
 
 
