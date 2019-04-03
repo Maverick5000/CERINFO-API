992883a53010e94580f4da1d35e790fb27197fbe
André Kraüss <andre.kmbv@gmail.com>
2019-04-03 00:08:56 -0400

    Test model

--- app/models/libro.rb
+++ app/models/libro.rb
@@ -1,9 +1,9 @@
 class Libro < ApplicationRecord
-  belongs_to :autor
-  belongs_to :genero
-  belongs_to :idioma
-  belongs_to :material
-  belongs_to :sigtop
-  belongs_to :editorial
+  belongs_to :autor, foreign_key: :autor
+  belongs_to :genero, foreign_key: :genero
+  belongs_to :idioma, foreign_key: :idioma
+  belongs_to :material, foreign_key: :material
+  belongs_to :sigtop, foreign_key: :sigtop
+  belongs_to :editorial, foreign_key: :editorial
   has_paper_trail
 end
